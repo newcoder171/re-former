5e5e5ab0b0d42eeaf192e716dcb98ec407221e23
newcoder <newcoder17@gmail.com>
2019-04-16 01:21:39 +0100

    fix edit and error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.new(user_params)
+        @user = User.find(params[:id])
+
         if @user.update(user_params)
             redirect_to new_user_path
         else
